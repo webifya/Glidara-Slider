4d16e37f1617171cb1a3302bf463eec4deab8c0d
Mahfuzar Rahman <161525559+webifya@users.noreply.github.com>
2026-08-02 01:13:13 +0600

    Release 2.2.2: update glidara-slider.php

--- glidara-slider.php
+++ glidara-slider.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Glidara Slider
  * Description: A fast, responsive slider builder with images, text, video, HTML and shortcodes.
- * Version: 1.0.0
+ * Version: 2.2.2
  * Author: Webifya
  * License: GPL-2.0-or-later
  * Text Domain: glidara-slider
@@ -10,11 +10,21 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'GLIDARA_SLIDER_VERSION', '1.0.0' );
+define( 'GLIDARA_SLIDER_VERSION', '2.2.2' );
 define( 'GLIDARA_SLIDER_FILE', __FILE__ );
 define( 'GLIDARA_SLIDER_DIR', plugin_dir_path( __FILE__ ) );
 define( 'GLIDARA_SLIDER_URL', plugin_dir_url( __FILE__ ) );
 
 require_once GLIDARA_SLIDER_DIR . 'includes/class-glidara-slider-plugin.php';
+require_once GLIDARA_SLIDER_DIR . 'includes/class-glidara-slider-tools.php';
 
 Glidara_Slider_Plugin::instance();
+Glidara_Slider_Tools::instance();
+
+if ( ! function_exists( 'glidara_slider' ) ) {
+	function glidara_slider( $id, $echo = true ) {
+		$output = do_shortcode( '[glidara_slider id="' . absint( $id ) . '"]' );
+		if ( $echo ) echo $output; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		return $output;
+	}
+}
